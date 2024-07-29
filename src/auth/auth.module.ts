ff3674dd4ad66ed107059537fa5888e23fff98a6
Pluzhnikov Grigorii <pluzhnikovga@gmail.com>
2024-07-29 13:54:07 +0200

    Create NestJS app and first request

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { UserModel } from 'src/user/user.model';
+
+@Module({
+  controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: UserModel,
+        schemaOptions: {
+          collection: 'User',
+        },
+      },
+    ]),
+  ],
+  providers: [AuthService],
+})
+export class AuthModule {}
